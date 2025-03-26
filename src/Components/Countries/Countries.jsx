f4dd9bb0b4c2f6190130a4c286eacc06d4283628
sifat tarafder <tmdsifat98@gmail.com>
2025-03-27 03:59:17 +0600

    somethimg changed

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -3,18 +3,22 @@ import Country from "./Country";
 
 const Countries = ({ fetchData }) => {
   const countries = use(fetchData);
-const [flag, setFlag]=useState([])
-const handleFlag=(flag)=>{
-    console.log('flag eseche',flag);
-}
+  const [flag, setFlag] = useState([]);
+  const handleFlag = (newFlags) => {
+    const newVisitedFlag = [...flag, newFlags];
+    setFlag(newVisitedFlag);
+  };
   return (
     <div>
       <h1>Total Countries: {countries.length}</h1>
-      
+        <h2>visit:{flag.length}</h2>
       <div className="art">
         {countries.map((country) => (
-            
-          <Country handleFlag={handleFlag} key={country.cca3} country={country}></Country>
+          <Country
+            handleFlag={handleFlag}
+            key={country.cca3}
+            country={country}
+          ></Country>
         ))}
       </div>
     </div>
